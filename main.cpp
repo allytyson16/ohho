fe5a8ce84594367b7d7c640b80de9bdae3b7043b
allytyson16 <adelaideyebaoh2@gmail.com>
2021-08-09 15:30:58 +0000

    Who lives in a pineapple under the sea? Exhorbant and yellow and porish is he.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,27 @@
 #include <iostream>
 
 using namespace std;
+int addtwonumbers(int a, int b)
+{
+
+    return(a + b);
+
+}
+
 
 int main()
 {
-    cout << "Oh Christmas tree, o Christmas tree, Among the tree so love thee." << endl;
+    int a, b,z;
+    cout <<"Enter your value for a:" ;
+    cin >> a;
+    cout << "Enter value for b: " ;
+    cin >> b;
+
+    z = addtwonumbers(a, b);
+
+    cout <<"Your  final answer is: " <<z ;
+
+
     return 0;
 }
+
